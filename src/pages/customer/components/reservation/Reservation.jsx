9ea457e8eda8c725c55ffbaf4737a66f8b78ce98
nikolas iliopoulos <nikolasil2000@gmail.com>
2024-03-03 12:55:08 +0200

    fixed sizing for mobile

--- src/pages/customer/components/reservation/Reservation.jsx
+++ src/pages/customer/components/reservation/Reservation.jsx
@@ -8,91 +8,139 @@ export default function Reservation({ language }) {
     setList(items.find((i) => i.language === language.id).data);
   }, [language]);
   return (
-    <div className="reservation" id="reservation">
+    <div className={'reservation ' + (isMobile && 'mobile')} id="reservation">
       <h1>{list.title}</h1>
       <div className="container">
         {isMobile ? (
-          <>
+          <div>
             <h3>{list.subTitle1}</h3>
             <h3>{list.subTitle2}</h3>
-          </>
+          </div>
         ) : (
-          <>
+          <div>
             <h2>{list.subTitle1}</h2>
             <h2>{list.subTitle2}</h2>
-          </>
+          </div>
         )}
-
-        <div className={'platforms ' + (isMobile && 'mobile')}>
-          <div
-            className={'platform ' + (isMobile && 'mobile')}
-            onClick={() => {
-              window.open(
-                'https://airbnb.com/h/seasidehouseinneamakri',
-                '_blank'
-              );
-            }}
-          >
-            <img
-              src="assets/airbnb.png"
-              width="150"
-              height="100"
-              alt="airbnb"
-              title="airbnb"
-              loading="lazy"
-            />
+        <div className="platforms-container">
+          <div className="platforms">
+            <div
+              className={'platform ' + (isMobile && 'mobile')}
+              onClick={() => {
+                window.open(
+                  'https://airbnb.com/h/seasidehouseinneamakri',
+                  '_blank'
+                );
+              }}
+            >
+              <img
+                src="assets/airbnb.png"
+                width="150"
+                height="100"
+                alt="airbnb"
+                title="airbnb"
+                loading="lazy"
+              />
+            </div>
+            <div
+              className={'platform ' + (isMobile && 'mobile')}
+              onClick={() => {
+                window.open(
+                  'https://booking.com/hotel/gr/seaside-house-in-nea-makri.el.html',
+                  '_blank'
+                );
+              }}
+            >
+              <img
+                src="assets/booking.png"
+                width="150"
+                height="100"
+                alt="booking"
+                title="booking"
+                loading="lazy"
+              />
+            </div>
+            {!isMobile && (
+              <>
+                <div
+                  className={'platform ' + (isMobile && 'mobile')}
+                  onClick={() => {
+                    window.open(
+                      'https://www.vrbo.com/en-ca/cottage-rental/p3575497vb',
+                      '_blank'
+                    );
+                  }}
+                >
+                  <img
+                    src="assets/vrbo.png"
+                    width="150"
+                    height="100"
+                    alt="vrbo"
+                    title="vrbo"
+                    loading="lazy"
+                  />
+                </div>
+                <div
+                  className={'platform ' + (isMobile && 'mobile')}
+                  onClick={() => {
+                    window.open(
+                      'https://bnbingreece.com/en/room/10156',
+                      '_blank'
+                    );
+                  }}
+                >
+                  <img
+                    src="assets/bnbingreece.png"
+                    width="110"
+                    height="110"
+                    alt="bnbingreece"
+                    title="bnbingreece"
+                    loading="lazy"
+                  />
+                </div>
+              </>
+            )}
           </div>
-          <div
-            className={'platform ' + (isMobile && 'mobile')}
-            onClick={() => {
-              window.open(
-                'https://booking.com/hotel/gr/seaside-house-in-nea-makri.el.html',
-                '_blank'
-              );
-            }}
-          >
-            <img
-              src="assets/booking.png"
-              width="150"
-              height="100"
-              alt="booking"
-              title="booking"
-              loading="lazy"
-            />
-          </div>
-          <div
-            className={'platform ' + (isMobile && 'mobile')}
-            onClick={() => {
-              window.open(
-                'https://www.vrbo.com/en-ca/cottage-rental/p3575497vb',
-                '_blank'
-              );
-            }}
-          >
-            <img
-              src="assets/vrbo.png"
-              width="150"
-              height="100"
-              alt="vrbo"
-              title="vrbo"
-              loading="lazy"
-            />
-          </div>
-          <div
-            className={'platform ' + (isMobile && 'mobile')}
-            onClick={() => {
-              window.open('https://bnbingreece.com/en/room/10156', '_blank');
-            }}
-          >
-            <img
-              src="assets/bnbingreece.png"
-              width="110"
-              height="110"
-              alt="bnbingreece"
-              title="bnbingreece"
-              loading="lazy"
-            />
-          </div>
+          {isMobile && (
+            <div className="platforms">
+              <div
+                className={'platform ' + (isMobile && 'mobile')}
+                onClick={() => {
+                  window.open(
+                    'https://www.vrbo.com/en-ca/cottage-rental/p3575497vb',
+                    '_blank'
+                  );
+                }}
+              >
+                <img
+                  src="assets/vrbo.png"
+                  width="150"
+                  height="100"
+                  alt="vrbo"
+                  title="vrbo"
+                  loading="lazy"
+                />
+              </div>
+              <div
+                className={'platform ' + (isMobile && 'mobile')}
+                onClick={() => {
+                  window.open(
+                    'https://bnbingreece.com/en/room/10156',
+                    '_blank'
+                  );
+                }}
+              >
+                <img
+                  src="assets/bnbingreece.png"
+                  width="110"
+                  height="110"
+                  alt="bnbingreece"
+                  title="bnbingreece"
+                  loading="lazy"
+                />
+              </div>
+            </div>
+          )}
         </div>
       </div>
       <div></div>
